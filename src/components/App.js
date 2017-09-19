3aa8690474922504cfdd2c99065e27bb4f549497
XiNGRZ <chenxingyu92@gmail.com>
2017-09-19 21:42:08 +0800

    looks better

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,21 @@ import {
 
 import Releases from './Releases';
 
+const alphaStyle = {
+  fontSize: 12,
+  fontFamily: 'monospace',
+  fontWeight: 'normal',
+  top: '-1em',
+};
+
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <AppBar title="CAF Tracker" />
+        <AppBar title={<span>
+          CAF Tracker <sup style={alphaStyle}>{"{alpha}"}</sup>
+        </span>} />
         <Releases releases={this.props.releases} />
       </div>
     );

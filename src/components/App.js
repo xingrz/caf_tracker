e799957d90b7ae07da6353dd149ed97e9493cedb
XiNGRZ <chenxingyu92@gmail.com>
2017-09-21 02:16:56 +0800

    Fork me on GitHub
    Change-Id: I6bcba3323d83e0b4fabcab5952eb8f18ecc9a48a

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 
 import {
   AppBar,
+  IconButton,
 } from 'material-ui';
+
+import { white } from 'material-ui/styles/colors';
+
+import GitHub from 'mui-icons/cmdi/github';
 
 const alphaStyle = {
   fontSize: 12,
@@ -11,14 +17,31 @@ const alphaStyle = {
   top: '-1em',
 };
 
+@withRouter
 export default class App extends Component {
 
   render() {
+    const title = (
+      <span>
+        CAF Tracker <sup style={alphaStyle}>{"{alpha}"}</sup>
+      </span>
+    );
+
+    const actions = (
+      <span>
+        <IconButton
+          tooltip="Fork me on GitHub"
+          tooltipPosition="bottom-left"
+          onClick={() => window.open('https://github.com/MoKee/caf_tracker')}
+        >
+          <GitHub color={white} />
+        </IconButton>
+      </span>
+    );
+
     return (
       <div>
-        <AppBar title={<span>
-          CAF Tracker <sup style={alphaStyle}>{"{alpha}"}</sup>
-        </span>} />
+        <AppBar title={title} iconElementRight={actions} />
         {this.props.children}
       </div>
     );

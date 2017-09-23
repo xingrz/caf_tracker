92ad8298b7d47590590f0c59422b0e4cbbe09d70
XiNGRZ <chenxingyu92@gmail.com>
2017-09-23 13:46:32 +0800

    db: Don't log sql unless starts with `--log-sql` flag
    Change-Id: Ib1d2cc4fcab1e7e5bb16316bdac098c33f4f8143

--- src/db.js
+++ src/db.js
@@ -1,5 +1,10 @@
 import Sequelize from 'sequelize';
 
 const DB = process.env.DB || 'mysql://root@localhost/caf';
+const LOG = process.argv.includes('--log-sql');
 
-export default new Sequelize(DB);
+const logging = LOG
+  ? log => console.log(log)
+  : noop => noop;
+
+export default new Sequelize(DB, { logging });
